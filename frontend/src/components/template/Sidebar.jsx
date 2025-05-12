d565d3a1919be7415c6f700c3b36d1877e2f0219
Laura Vidal <lauravidal@outlook.com>
2025-05-12 16:35:46 -0300

    deixando a logo clicavel pra home

--- frontend/src/components/template/Sidebar.jsx
+++ frontend/src/components/template/Sidebar.jsx
@@ -8,20 +8,20 @@ import {
 } from 'react-icons/fa';
 
 export const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <div className="logo">
+  return (
+      <div className="sidebar">
+          <Link to="/" className="logo" style={{ textDecoration: 'none', color: 'inherit' }}>
               <FaTooth className="logo-icon" />
               <span>Clínica+ Saúde</span>
-            </div>
-            <ul className="nav-list">
+          </Link>
+          <ul className="nav-list">
               <li><Link to="/"><FaHome className="icon" /> Homepage</Link></li>
               <li><Link to="/pacientes"><FaUserInjured className="icon" /> Pacientes</Link></li>
               <li><Link to="/consultas"><FaClipboardList className="icon" /> Consultas</Link></li>
               <li><Link to="/calendario"><FaCalendarAlt className="icon" /> Calendário</Link></li>
               <li><Link to="/material"><FaBoxOpen className="icon" /> Material</Link></li>
-            </ul>
-            <div className="logout"><FaSignOutAlt className="icon" /> Sair</div>
-        </div>
-    )
+          </ul>
+          <div className="logout"><FaSignOutAlt className="icon" /> Sair</div>
+      </div>
+  )
 }
