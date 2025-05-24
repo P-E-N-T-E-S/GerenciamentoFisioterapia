95c09cd1613c549511519d1809e6b8def7ee13a6
Nerebo <andregoesf@gmail.com>
2025-05-24 15:23:00 -0300

    Where we started from the bottom

--- infraestrutura/src/main/java/com/implantodontia/infraestrutura/persistencia/core/gestao/gestaopaciente/FichaMedicaJPA.java
+++ infraestrutura/src/main/java/com/implantodontia/infraestrutura/persistencia/core/gestao/gestaopaciente/FichaMedicaJPA.java
@@ -1,25 +1,29 @@
-package com.implantodontia.infraestrutura.persistencia.core.administracao.paciente;
+package com.implantodontia.infraestrutura.persistencia.core.gestao;
 
+import com.implantodontia.dominio.core.gestaoPacientes.paciente.Paciente;
+import com.implantodontia.infraestrutura.persistencia.core.administracao.paciente.PacienteJPA;
+import com.implantodontia.infraestrutura.persistencia.core.administracao.usuario.UsuarioJPA;
+import com.implantodontia.infraestrutura.persistencia.core.paciente.PacienteJPA;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "ficha_medica")
 public class FichaMedicaJPA {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "paciente_id")
+    private PacienteJPA paciente;
 
     private String historicoMedico;
     private String alergias;
     private String observacoes;
     private LocalDateTime ultimaAtualizacao;
-
-    @OneToOne
-    @JoinColumn(name = "paciente_id")
-    private PacienteJPA paciente;
 
     public Long getId() {
         return id;
@@ -53,19 +57,19 @@ public class FichaMedicaJPA {
         this.observacoes = observacoes;
     }
 
-    public LocalDateTime getUltimaAtualizacao() {
-        return ultimaAtualizacao;
-    }
-
-    public void setUltimaAtualizacao(LocalDateTime ultimaAtualizacao) {
-        this.ultimaAtualizacao = ultimaAtualizacao;
-    }
-
     public PacienteJPA getPaciente() {
         return paciente;
     }
 
     public void setPaciente(PacienteJPA paciente) {
         this.paciente = paciente;
+    }
+
+    public LocalDateTime getUltimaAtualizacao() {
+        return ultimaAtualizacao;
+    }
+
+    public void setUltimaAtualizacao(LocalDateTime ultimaAtualizacao) {
+        this.ultimaAtualizacao = ultimaAtualizacao;
     }
 }
